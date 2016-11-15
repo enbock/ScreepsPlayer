c8b1db0f324f20d4b1575ae0feda97d8e1dc1021
Gandur <github@itbock.de>
2016-11-15 16:15:21 +0100

    Update Creep.Action.None.js

--- World/Creep.Action.None.js
+++ World/Creep.Action.None.js
@@ -1,5 +1,5 @@
 module.exports = function(myCreep) {
     var creep = myCreep.Me;
     if(Math.random() <= 0.4 && Math.random() >= 0.6) creep.say("zzZ");
-    myCreep.Move(myCreep.Logistic.Spawn);
+    myCreep.SetAction("Fill.Controller");
 };
